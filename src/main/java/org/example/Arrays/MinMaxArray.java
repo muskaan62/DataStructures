116b1002ff7df75b8192ba553ebef34bf40dd3ee
muskaan62 <muskaanbegum23@gmail.com>
2025-07-16 21:57:11 +0530

    july 16

---
+++ src/main/java/org/example/Arrays/MinMaxArray.java
@@ -0,0 +1,73 @@
+package org.example.Arrays;
+
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int arr[] = {0,0,1,2};
+        System.out.println("Find minimum and maximum in an array");
+        printArray(findMinMAx(arr));
+        System.out.println("Find minimum and maximum in an array using two pointer approach");
+        printArray(findMinMaxTwoPointer(arr));
+
+        System.out.println("Min Array using recursion " + findMin(arr, arr.length));
+        System.out.println("Max Array using recursion " + findMax(arr, arr.length));
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i=0;i<=arr.length-1;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // using brute force approach
+    public static int[] findMinMAx(int arr[]) {
+        int min=arr[0];
+        int max=arr[0];
+        for (int i=0;i<=arr.length-1;i++) {
+            if (arr[i]>=max) {
+                max =arr[i];
+            }
+            if (arr[i]<=min) {
+                min = arr[i];
+            }
+        }
+        return new int[]{min,max};
+    }
+
+    // using two pointer approach
+    public static int[] findMinMaxTwoPointer(int arr[]) {
+       int left = 0;
+       int right = arr.length-1;
+       int min = arr[0];
+       int max = arr [0];
+       while (left<=right) {
+           if (arr[left]<min) {
+               min = arr[left];
+           }
+           if (arr[right]<min) {
+               min = arr[right];
+           }
+           if (arr[left]>max) {
+               max = arr[left];
+           }
+           if (arr[right]>max) {
+               max = arr[right];
+           }
+           left++;
+           right--;
+       }
+       return new int[]{min,max};
+    }
+
+    public static int findMin(int arr[], int n) {
+        if (n==0)
+            return arr[0];
+        return Math.min(arr[n-1], findMin(arr, n-1));
+    }
+
+    public static int findMax(int arr[], int n) {
+        if (n == 0)
+            return arr[0];
+        return Math.max(arr[n-1], findMax(arr, n-1));
+    }
+}
--- src/main/java/org/example/Arrays/ReverseArray.java
+++ src/main/java/org/example/Arrays/ReverseArray.java
@@ -1,4 +1,61 @@
 package org.example.Arrays;
 
 public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {5,7,9,2,8};
+        System.out.println("Reverse array");
+        int[] revArray = reverse(arr);
+        printArray(revArray);
+        System.out.println("Reverse array using two pointer approach");
+        int[] revArrayTwoPointer = reverseTwoPinterArray(arr);
+        printArray(revArrayTwoPointer);
+        System.out.println("Reverse array using recursion");
+        System.out.println("before array reversal");
+        printArray(arr);
+        reverseArrayRecursion(arr, 0, arr.length-1);
+        System.out.println("after array reversal");
+        printArray(arr);
+
+    }
+    public static void printArray(int[] arr) {
+        for (int i=0;i<=arr.length-1;i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    // using brute force approach
+    public static int[] reverse(int[] arr){
+        int n= arr.length;
+        int[] result = new int[n];
+        for (int i = 0;i<=n-1;i++) {
+            result[i] = arr[n-1-i];
+        }
+        return result;
+    }
+
+    // using two pointer approach
+    public static int[] reverseTwoPinterArray(int[] arr) {
+        int n= arr.length;
+        int left = 0;
+        int right = n-1;
+        while (left <= right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left ++;
+            right --;
+        }
+        return arr;
+    }
+
+    // using Recursion
+    public static void reverseArrayRecursion(int[] arr, int left, int right) {
+        if (left<=right) {
+            return ;
+        }
+        int temp = arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+        reverseArrayRecursion(arr,left+1,right-1);
+    }
 }
