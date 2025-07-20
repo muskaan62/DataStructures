24d23ba7d6bbac9ac54ec2ff698eea243d042925
muskaan62 <muskaanbegum23@gmail.com>
2025-07-20 12:30:41 +0530

    search and sort algorithm

---
+++ src/main/java/org/example/Algorithms/Search/BinarySearch.java
@@ -0,0 +1,49 @@
+package org.example.Algorithms.Search;
+
+//You are given a sorted array of n integers and a target value x.
+// Your task is to write a function that returns the index of x in the array using binary search. If x is not present, return -1.
+//Example:
+//Input: arr = [1, 3, 5, 7, 9, 11], target = 7
+//Output: 3
+//Input: arr = [1, 3, 5, 7, 9, 11], target = 6
+//Output: -1
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 7, 9, 11};
+        int target = 9;
+        System.out.println("Binary Search using brute force approach: " + binarySearch(arr, target));
+        System.out.println("Binary Search using recursion: " + find(arr, target, 0, arr.length - 1));
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            if (arr[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int find(int[] arr, int target, int low, int high) {
+        if (low > high)
+            return -1;
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (arr[mid] < target) {
+            return find(arr, target, mid + 1, high);
+        } else {
+            return find(arr, target, low, mid - 1);
+        }
+    }
+}
---
+++ src/main/java/org/example/Algorithms/Sort/BubbleSort.java
@@ -0,0 +1,55 @@
+package org.example.Algorithms.Sort;
+
+//Implement the Bubble Sort algorithm to sort an array of integers in ascending order.4
+//The Bubble Sort algorithm compares adjacent elements in the array and swaps them if they are in the wrong order.
+// This process is repeated until the entire array is sorted.
+// Example:
+//Input:
+//nums = [5, 2, 9, 1, 5, 6]
+//Output:
+//[1, 2, 5, 5, 6, 9]
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+        System.out.println("Array before sorting:");
+        printArray(arr);
+        bubbleSort(arr);
+        System.out.println("Array after sorting:");
+        printArray(arr);
+        System.out.println("Array sorted in descending order:");
+        bubbleSortDesc(arr);
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    // sort in asscending order
+    public static void bubbleSort(int[] arr) {
+        for (int i=0;i<arr.length;i++) {
+            for (int j=i+1;j<arr.length;j++) {
+                if (arr[i]>arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void bubbleSortDesc(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
